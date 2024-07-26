ddaaed2e3917aa6003b78498d8cc69cb9392e5fa
bboggo <kmt0819@naver.com>
2024-07-27 03:44:13 +0900

    init

---
+++ src/main/java/com/backend/devfordev/DevfordevApplication.java
@@ -0,0 +1,13 @@
+package com.backend.devfordev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevfordevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevfordevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/devfordev/DevfordevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.devfordev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevfordevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

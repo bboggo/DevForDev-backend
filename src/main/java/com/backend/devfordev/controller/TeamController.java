e50f829d94cae0491b8a419a894dc15a651e3a82
bboggo <kmt0819@naver.com>
2024-12-04 02:06:31 +0900

    #36 fix: teamId url 수정

--- src/main/java/com/backend/devfordev/controller/TeamController.java
+++ src/main/java/com/backend/devfordev/controller/TeamController.java
@@ -70,7 +70,7 @@ public class TeamController {
 
 
     @Operation(summary = "팀 모집글 상세 조회")
-    @GetMapping(value = "/v1/team/{id}")
+    @GetMapping(value = "/v1/team/{teamId}")
     public ResponseEntity<ApiResponse<TeamResponse.TeamDetailResponse>> getTeamDetail(@PathVariable Long id) {
         TeamResponse.TeamDetailResponse teamDetail = teamService.getTeamDetail(id);
 
@@ -97,7 +97,7 @@ public class TeamController {
 
 
     @Operation(summary = "팀 모집글 삭제")
-    @DeleteMapping(value = "/v1/team/{id}")
+    @DeleteMapping(value = "/v1/team/{teamId}")
     public ResponseEntity<ApiResponse> deleteTeam(@PathVariable Long id, @AuthenticationPrincipal User user) {
         teamService.deleteTeam(id, Long.parseLong(user.getUsername()));
         ApiResponse apiResponse = ApiResponse.builder()
@@ -160,7 +160,7 @@ public class TeamController {
 
 
     @Operation(summary = "팀 모집 공고 업데이트", description = "팀 모집 공고를 업데이트합니다.")
-    @PatchMapping("/{teamId}")
+    @PatchMapping("v1/team/{teamId}")
     public ResponseEntity<ApiResponse<TeamResponse.TeamUpdateResponse>> updateTeam(
             @PathVariable Long teamId,
             @RequestBody @Valid TeamRequest.TeamUpdateRequest request,

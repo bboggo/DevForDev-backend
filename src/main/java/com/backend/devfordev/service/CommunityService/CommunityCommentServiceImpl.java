c899dddf3b0e89336e9d756233c4c4e33c4e8188
bboggo <kmt0819@naver.com>
2024-12-26 13:49:51 +0900

    #96 feat: 댓글 등록 api

---
+++ src/main/java/com/backend/devfordev/service/CommunityService/CommunityCommentServiceImpl.java
@@ -0,0 +1,54 @@
+package com.backend.devfordev.service.CommunityService;
+
+import com.backend.devfordev.converter.CommunityConverter;
+import com.backend.devfordev.domain.CommunityEntity.Community;
+import com.backend.devfordev.domain.CommunityEntity.CommunityComment;
+import com.backend.devfordev.domain.MemberEntity.Member;
+import com.backend.devfordev.dto.CommunityDto.CommunityCommentRequest;
+import com.backend.devfordev.dto.CommunityDto.CommunityCommentResponse;
+import com.backend.devfordev.repository.CommunityRepository.CommunityCommentRepository;
+import com.backend.devfordev.repository.CommunityRepository.CommunityRepository;
+import com.backend.devfordev.repository.MemberRepository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CommunityCommentServiceImpl implements CommunityCommentService{
+
+    private final CommunityCommentRepository commentRepository;
+    private final CommunityRepository communityRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public CommunityCommentResponse addComment(Long comId, Long userId, CommunityCommentRequest request) {
+        Community community = communityRepository.findById(comId)
+                .orElseThrow(() -> new IllegalArgumentException("Community not found"));
+
+        Member member = memberRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Member not found"));
+
+        CommunityComment parentComment = null;
+        if (request.getParentId() != null) {
+            parentComment = commentRepository.findById(request.getParentId())
+                    .orElseThrow(() -> new IllegalArgumentException("Parent comment not found"));
+        }
+
+        CommunityComment comment = CommunityComment.builder()
+                .community(community)
+                .member(member)
+                .commentContent(request.getContent())
+                .parent(parentComment)
+                .build();
+
+        CommunityComment savedComment = commentRepository.save(comment);
+
+        return CommunityConverter.toCommunityCommentResponse(savedComment);
+    }
+
+
+}
